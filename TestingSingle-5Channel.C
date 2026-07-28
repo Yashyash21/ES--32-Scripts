dfac9a7c66a53ab778c47318bbaf86958dc853c5
yashyash21 <yashdhawale17@gmail.com>
2026-07-28 11:39:11 +0530

    ADD

---
+++ TestingSingle-5Channel.C
@@ -0,0 +1,37 @@
+// Relay Pins
+const int relayPins[] = {5, 18, 19, 21, 22};
+const int relayCount = sizeof(relayPins) / sizeof(relayPins[0]);
+
+void setup() {
+  Serial.begin(115200);
+
+  for (int i = 0; i < relayCount; i++) {
+    pinMode(relayPins[i], OUTPUT);
+    digitalWrite(relayPins[i], LOW);   // OFF
+  }
+
+  Serial.println("Sequential Relay Test Started...");
+}
+
+void loop() {
+
+  // Turn ON one by one
+  for (int i = 0; i < relayCount; i++) {
+    Serial.print("Relay ");
+    Serial.print(i + 1);
+    Serial.println(" ON");
+
+    digitalWrite(relayPins[i], HIGH);
+    delay(5000);
+  }
+
+  // Turn OFF one by one
+  for (int i = 0; i < relayCount; i++) {
+    Serial.print("Relay ");
+    Serial.print(i + 1);
+    Serial.println(" OFF");
+
+    digitalWrite(relayPins[i], LOW);
+    delay(5000);
+  }
+}
